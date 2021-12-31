777e1df5a202d4d27da27b41010f140c1eb291af
Oleg Sucharevich <oleg2807@gmail.com>
2021-12-31 18:33:41 +0200

    feat: store docs in memory

--- pkg/db/memory.go
+++ pkg/db/memory.go
@@ -4,25 +4,36 @@ import (
 	"context"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/olegsu/go-tools/pkg/logger"
 )
 
 type (
 	memoryDal struct {
-		lgr *logger.Logger
+		lgr      *logger.Logger
+		expenses map[string]Document
+		raw      map[string]RawDocument
 	}
 )
 
 func (m *memoryDal) Create(ctx context.Context, date time.Time, doc Document) (string, error) {
-	return "", nil
+	u, _ := uuid.NewUUID()
+	m.expenses[u.String()] = doc
+	return u.String(), nil
 }
 func (m *memoryDal) CreateRaw(ctx context.Context, date time.Time, doc RawDocument) (string, error) {
-	return "", nil
+	u, _ := uuid.NewUUID()
+	m.raw[u.String()] = doc
+	return u.String(), nil
 }
 func (m *memoryDal) Close() error {
 	return nil
 }
 
 func newMemoryDal(ctx context.Context, options options) (*memoryDal, error) {
-	return &memoryDal{}, nil
+	return &memoryDal{
+		lgr:      options.lgr,
+		expenses: map[string]Document{},
+		raw:      map[string]RawDocument{},
+	}, nil
 }
