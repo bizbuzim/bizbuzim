2c9d4cdff59ad7a063c4d9816c739d1f4952193f
Oleg Sucharevich <oleg2807@gmail.com>
2022-01-02 21:37:31 +0200

    feat: add pg queries

---
+++ db/queries/expenses.sql
@@ -0,0 +1,8 @@
+-- name: CreateExpense :exec
+INSERT INTO expenses (id, name, payment, price, tags, description, "createdAt", "createdBy")
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
+
+-- name: CreateRawExpense :exec
+INSERT INTO raw_expenses (id, text, "createdAt", "createdBy")
+    VALUES ($1, $2, $3, $4);
+
