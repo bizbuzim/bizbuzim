bf7dbce57cd0f20ed2d78b82025f8b37b80b5dbd
Oleg Sucharevich <oleg2807@gmail.com>
2022-01-05 19:42:10 +0200

    fix: connection to psql from cloud run

--- graphile/index.js
+++ graphile/index.js
@@ -1,33 +1,19 @@
 const express = require("express");
 const { postgraphile } = require("postgraphile");
-const Knex = require('knex');
 
 const app = express();
 
-const createUnixSocketPool = async config => {
-  const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
-  const host = `${dbSocketPath}/${process.env.INSTANCE_CONNECTION_NAME}`
-  return Knex({
-    client: 'pg',
-    connection: {
-      user: process.env.POSTGRES_USER, 
-      password: process.env.POSTGRES_PASSWORD, 
-      database: process.env.POSTGRES_DATABASE, 
-      host,
-    },
-    ...config,
-  });
-};
-let buildURI = () => {
+
+const buildURI = () => {
   const user = process.env.POSTGRES_USER;
   const password = process.env.POSTGRES_PASSWORD;
   const database = process.env.POSTGRES_DATABASE;
-  const port = process.env.POSTGRES_PORT;
-  const host = process.env.POSTGRES_HOST;
-  return `postgres://${user}:${password}@${host}:${port}/${database}?sslmode=disable`
-}
-if (process.env.INSTANCE_CONNECTION_NAME) {
-  buildURI = createUnixSocketPool
+  let host = `${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}`;
+  if (process.env.INSTANCE_CONNECTION_NAME) {
+    const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+    host = `${dbSocketPath}/${process.env.INSTANCE_CONNECTION_NAME}`
+  }
+  return `postgres://${user}:${password}@${host}/${database}?sslmode=disable`
 }
 app.use(
   postgraphile(
