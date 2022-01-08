704dd33b275adff1915fb9b578104aad1d186223
Oleg Sucharevich <oleg2807@gmail.com>
2022-01-08 11:13:54 +0200

    refactor: connect to db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -20,34 +20,9 @@ func main() {
 	lgr := logger.New()
 	lgr.Info("starting server")
 
-	user := fatal.GetEnv("POSTGRES_USER")
-	password := fatal.GetEnv("POSTGRES_PASSWORD")
-	dbname := fatal.GetEnv("POSTGRES_DATABASE")
-
-	uri := ""
-	gcp := os.Getenv("INSTANCE_CONNECTION_NAME")
-	if gcp != "" {
-		lgr.Info("connecting to gcp sql server")
-		socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
-		if !isSet {
-			lgr.Info("socket dir is not set")
-			socketDir = "/cloudsql"
-		}
-		uri = fmt.Sprintf("user=%s password=%s database=%s host=%s/%s", user, password, dbname, socketDir, gcp)
-	} else {
-		lgr.Info("connecting to postgres directly")
-		host := fatal.GetEnv("POSTGRES_HOST")
-		port := fatal.GetEnv("POSTGRES_PORT")
-		uri = fmt.Sprintf("host=%s port=%s user=%s "+
-			"password=%s dbname=%s sslmode=disable",
-			host, port, user, password, dbname)
-	}
-
-	db, err := sql.Open("postgres", uri)
+	db, err := sql.Open("postgres", buildDatabaseConnURI(lgr))
 	dieOnError(err, "failed to connect to db")
-
 	dieOnError(db.Ping(), "failed to ping to the database")
-
 	lgr.Info("connected to db")
 
 	bot, err := tgbotapi.NewBotAPI(fatal.GetEnv("TELEGRAM_BOT_TOKEN"))
@@ -77,10 +52,35 @@ func main() {
 
 	}
 	http.HandleFunc("/", handlers.MessageHandler(lgr, bot, db))
-	err = http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8000", nil)
 	dieOnError(err, "failed to start server")
 }
 
 func dieOnError(err error, msg string) {
 	fatal.DieOnError(err, msg)
+}
+func buildDatabaseConnURI(lgr *logger.Logger) string {
+	user := fatal.GetEnv("POSTGRES_USER")
+	password := fatal.GetEnv("POSTGRES_PASSWORD")
+	dbname := fatal.GetEnv("POSTGRES_DATABASE")
+	uri := ""
+	gcp := os.Getenv("INSTANCE_CONNECTION_NAME")
+	if gcp != "" {
+		lgr.Info("connecting to gcp sql server")
+		socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+		if !isSet {
+			lgr.Info("socket dir is not set")
+			socketDir = "/cloudsql"
+		}
+		uri = fmt.Sprintf("user=%s password=%s database=%s host=%s/%s", user, password, dbname, socketDir, gcp)
+	} else {
+		lgr.Info("connecting to postgres directly")
+		host := fatal.GetEnv("POSTGRES_HOST")
+		port := fatal.GetEnv("POSTGRES_PORT")
+		uri = fmt.Sprintf("host=%s port=%s user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+			host, port, user, password, dbname)
+	}
+
+	return uri
 }
