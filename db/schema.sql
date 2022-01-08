968dd4b11565a0a7ce55326d7e8811714b06259f
Oleg Sucharevich <oleg2807@gmail.com>
2022-01-08 11:04:59 +0200

    chore(pg_dump): regen schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@
 --
 
 -- Dumped from database version 14.1 (Debian 14.1-1.pgdg110+1)
--- Dumped by pg_dump version 14.1
+-- Dumped by pg_dump version 14.1 (Debian 14.1-1.pgdg110+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
