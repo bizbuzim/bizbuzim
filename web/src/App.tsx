256daae47a2d5df06901f5979f5692ef539b7807
Ortal Sucharevich <ortalsucharevich@Ortals-MacBook-Air.local>
2022-01-08 20:40:35 +0200

    use apollo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+
 
 function App() {
+  useEffect(() => {
+    const client = new ApolloClient({
+      uri: 'http://10.100.102.47:8080/api/v1/graphql',
+      cache: new InMemoryCache()
+    });
+    client
+  .query({
+    query: gql`
+    {
+      expenses {
+        name
+        payment
+        price
+        tags
+      }
+    }    
+    `
+  })
+  .then(result => console.log(result));
+  })
+
   return (
     <div className="App">
       
