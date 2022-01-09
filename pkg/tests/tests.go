419bdb9bc7932130fae67e053ce75421db4d7b5d
Oleg Sucharevich <oleg2807@gmail.com>
2022-01-09 21:17:15 +0200

    chore: add tests

---
+++ pkg/tests/tests.go
@@ -0,0 +1,14 @@
+package tests
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/olegsu/bizbuzim/pkg/fatal"
+)
+
+func MustParseTime(layout string, value string) time.Time {
+	t, err := time.Parse(layout, value)
+	fatal.DieOnError(err, fmt.Sprintf("failed to parse time: %s", value))
+	return t
+}
