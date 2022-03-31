329dab0122c1843ae0b59c1670f6d2c8e1995479
Oleg Sucharevich <oleg2807@gmail.com>
2022-03-31 08:26:36 +0300

    feat(web): add react router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import styled from "styled-components";
 import { ExpensesTable } from "./components/expenses-table";
+import { Home } from "./views/home";
+import { Routes, Route } from "react-router-dom";
 
 const Container = styled.div``;
 
 function App() {
   return (
     <Container className="app">
-      <ExpensesTable />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="expenses" element={<ExpensesTable />} />
+      </Routes>
     </Container>
   );
 }
