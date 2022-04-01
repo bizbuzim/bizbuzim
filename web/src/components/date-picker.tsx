b8cb18894d6a5020576ca4ae7af40345264a5752
Oleg Sucharevich <oleg2807@gmail.com>
2022-04-01 13:17:36 +0300

    feat(web): add init state

--- web/src/components/date-picker.tsx
+++ web/src/components/date-picker.tsx
@@ -4,9 +4,10 @@ import "react-datepicker/dist/react-datepicker.css";
 
 export interface PickerProps {
   onDateChanged: (date: Date) => void;
+  initial?: Date;
 }
 export function DatePicker(props: PickerProps) {
-  const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(props.initial || new Date());
   return (
     <Picker
       selected={startDate}
