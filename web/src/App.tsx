8440f4efd6d234e94f75e3f3e1971f5571d9d9fa
Oleg Sucharevich <oleg2807@gmail.com>
2022-04-01 13:13:12 +0300

    feat(web): organaize layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,15 +4,37 @@ import { Sidebar } from "./components/sidebar";
 import { Router } from "./components/router";
 
 const Container = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-areas:
+    "header header header header header header header header"
+    "menu main main main main main main main";
   height: 100vh;
+`;
+
+const HeaderContainer = styled.div`
+  grid-area: header;
+  height: 5vh;
+`;
+const MenuContainer = styled.div`
+  grid-area: menu;
+  height: 95vh;
+`;
+
+const MainContainer = styled.div`
+  grid-area: main;
+  height: 95vh;
 `;
 
 function App() {
   return (
     <Container className="app">
-      <Sidebar />
-      <Router />
+      <HeaderContainer>header</HeaderContainer>
+      <MenuContainer>
+        <Sidebar />
+      </MenuContainer>
+      <MainContainer>
+        <Router />
+      </MainContainer>
     </Container>
   );
 }
