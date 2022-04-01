1564a3b36ea733b9b249222bc15acb93bba154aa
Oleg Sucharevich <oleg2807@gmail.com>
2022-04-01 13:14:56 +0300

    feat(web): replace table with rows design

--- web/src/components/expenses-table.tsx
+++ web/src/components/expenses-table.tsx
@@ -1,97 +1,77 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import client from "./../services/gql";
 import { GET_ALL_EXPENSES } from "../queries/get-all-expenses";
 import styled from "styled-components";
-import { useTable } from "react-table";
-import { DatePicker } from "./date-picker";
-import { Divider } from "./divider";
 
 const Styles = styled.div`
   padding: 1rem;
   overflow: scroll;
-
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
 `;
 
-const DatePickerContainer = styled.div`
+const StyledLabeledText = styled.div`
+  text-align: center;
+  border-radius: 20px;
+  border-color: #ccc;
+  border-style: solid;
+  background-color: #ccc;
+  margin-right: 2px;
+  margin-top: 2px;
+  margin-bottom: 2px;
+  min-width: 60px;
+`;
+
+const StyledLabelsContainer = styled.div`
   display: inline-flex;
-  flex-direction: row;
-  gap: 1em;
 `;
-interface Options {
-  columns: any;
-  data: any;
-}
 
-function Table({ columns, data }: Options) {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({
-      columns,
-      data,
-    });
-
-  // Render the UI for your table
+const StyledRow = styled.div`
+  display: grid;
+  grid-template-columns: auto 30px auto;
+  min-height: 40px;
+  text-align: center;
+  grid-gap: 1rem;
+  border-style: solid;
+  border-color: #ccc;
+  border-radius: 10px;
+  margin: 10px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+`;
+function Row({ expense }: { expense: Expense }) {
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
+    <StyledRow key={expense.id}>
+      <div>{expense.name}</div>
+      <div>{expense.price}</div>
+      <StyledLabelsContainer>
+        {expense.tags.map((t, i) => {
           return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
-            </tr>
+            <StyledLabeledText key={i + Date.now()}>{t}</StyledLabeledText>
           );
         })}
-      </tbody>
-    </table>
+      </StyledLabelsContainer>
+    </StyledRow>
   );
 }
+interface Expense {
+  id: string;
+  name: string;
+  payment: string;
+  tags: string[];
+  price: string;
+}
 
-export function ExpensesTable() {
-  const [data, setData] = useState([]);
-  const [fromDate, setFromDate] = useState(new Date());
-  const [toDate, setToDate] = useState(new Date());
+interface Props {
+  dateFrom: Date;
+  dateTo: Date;
+}
+export function ExpensesTable(props: Props) {
+  const [data, setData] = useState<Expense[]>([]);
   useEffect(() => {
     const fetch = async () => {
       const res = await client.query({
         query: GET_ALL_EXPENSES,
         variables: {
-          from: fromDate,
-          to: toDate,
+          from: props.dateFrom,
+          to: props.dateTo,
         },
       });
 
@@ -99,53 +79,12 @@ export function ExpensesTable() {
     };
 
     fetch();
-  }, [fromDate, toDate]);
-  const columns = useMemo(
-    () => [
-      {
-        Header: "Table",
-        columns: [
-          {
-            Header: "Name",
-            accessor: "name",
-          },
-          {
-            Header: "Payment",
-            accessor: "payment",
-          },
-          {
-            Header: "Price",
-            accessor: "price",
-          },
-          {
-            Header: "Tags",
-            accessor: "tags",
-          },
-        ],
-      },
-    ],
-    []
-  );
+  }, [props.dateFrom, props.dateTo]);
   return (
     <Styles>
-      <DatePickerContainer>
-        From:
-        <DatePicker
-          onDateChanged={(d) => {
-            setFromDate(d);
-          }}
-        />
-      </DatePickerContainer>
-      <DatePickerContainer>
-        To:{" "}
-        <DatePicker
-          onDateChanged={(d) => {
-            setToDate(d);
-          }}
-        />
-      </DatePickerContainer>
-      <Divider />
-      <Table columns={columns} data={data} />
+      {data.map((v, i) => {
+        return <Row key={i} expense={v} />;
+      })}
     </Styles>
   );
 }
