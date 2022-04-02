298697a70b8ba48833afa29ffd41b6bedbc81869
Oleg Sucharevich <oleg2807@gmail.com>
2022-04-02 21:12:07 +0300

    chore(web): replace select

--- web/src/views/filters.tsx
+++ web/src/views/filters.tsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react";
 import _ from "lodash";
 import styled from "styled-components";
-import Select from "react-select";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Chip from "@mui/material/Chip";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import Box from "@mui/material/Box";
+import MenuItem from "@mui/material/MenuItem";
+
 import { BsFilter } from "react-icons/bs";
 import { DatePicker } from "./../components/date-picker";
 import client from "./../services/gql";
@@ -17,7 +23,19 @@ const FilterContainer = styled.div`
   margin: 1em;
   min-width: 10em;
   max-width: 15em;
+  max-height: 5em;
 `;
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 export interface Props {
   fromDate?: Date;
@@ -29,6 +47,7 @@ export function Filters(props: Props) {
   const [fromDate, setFromDate] = useState(props.fromDate || new Date());
   const [toDate, setToDate] = useState(props.toDate || new Date());
   const [tags, setTags] = useState<{ value: string; label: string }[]>([]);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
   useEffect(() => {
     const fetch = async () => {
       const res = await client.query({
@@ -73,7 +92,33 @@ export function Filters(props: Props) {
         />
       </FilterContainer>
       <FilterContainer>
-        Tags: w <Select options={tags} isMulti={true} />
+        <Select
+          multiple
+          value={selectedTags}
+          onChange={(event: SelectChangeEvent<string[]>) => {
+            const {
+              target: { value },
+            } = event;
+            setSelectedTags(
+              typeof value === "string" ? value.split(",") : value
+            );
+          }}
+          input={<OutlinedInput />}
+          renderValue={(selected: string[]) => (
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+              {selected.map((value: string) => (
+                <Chip key={value} label={value} />
+              ))}
+            </Box>
+          )}
+          MenuProps={MenuProps}
+        >
+          {tags.map((tag, i) => (
+            <MenuItem key={tag.label} value={tag.value}>
+              {tag.label}
+            </MenuItem>
+          ))}
+        </Select>
       </FilterContainer>
     </FiltersContainer>
   );
