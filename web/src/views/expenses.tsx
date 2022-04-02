c09a2f73dfb154b317177f2a3b3d3945215d5a7e
Oleg Sucharevich <oleg2807@gmail.com>
2022-04-02 10:54:13 +0300

    feat(web): add tag selection

--- web/src/views/expenses.tsx
+++ web/src/views/expenses.tsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import _ from "lodash";
 import styled from "styled-components";
 import { DatePicker } from "./../components/date-picker";
 import { ExpensesTable } from "./../components/expenses-table";
 import { Divider } from "./../components/divider";
+import Select from "react-select";
+import client from "./../services/gql";
+import { GET_TAGS } from "../queries/get-tags";
+
 const Container = styled.div`
   display: grid;
   grid-template-areas:
@@ -27,6 +32,32 @@ const DatePickerContainer = styled.div`
 export function Expenses() {
   const [fromDate, setFromDate] = useState(new Date("2022-03-01"));
   const [toDate, setToDate] = useState(new Date());
+  const [tags, setTags] = useState<{ value: string; label: string }[]>([]);
+  const [tagsQuery, setTagsQuery] = useState<any[]>([
+    { tags: { contains: ["חופשה"] } },
+    { tags: { contains: ["אוכל-בחוץ"] } },
+  ]);
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await client.query({
+        query: GET_TAGS,
+        variables: {
+          from: fromDate,
+          to: toDate,
+          tags: JSON.stringify(tagsQuery),
+        },
+      });
+      const t = _.map(res.data.expenses, (v) => v.tags);
+      const f = _.flatten(t);
+      const u = _.uniq(f);
+      setTags(
+        _.map(u, (vv) => {
+          return { value: vv, label: vv };
+        })
+      );
+    };
+    fetch();
+  }, [tags.length]);
   return (
     <Container>
       <FilterContainer>
@@ -48,6 +79,7 @@ export function Expenses() {
             }}
           />
         </DatePickerContainer>
+        <Select options={tags} isMulti={true} />
         <Divider />
       </FilterContainer>
       <TableContainer>
