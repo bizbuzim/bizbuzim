b1494c92844e535f5b75e1ea31e3f07e3231c73e
Oleg Sucharevich <oleg2807@gmail.com>
2022-04-02 19:30:51 +0300

    chore(web): replace date picker

--- web/src/components/date-picker.tsx
+++ web/src/components/date-picker.tsx
@@ -1,20 +1,31 @@
 import { useState } from "react";
-import Picker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import DateAdapter from "@mui/lab/AdapterDateFns";
+import Picker from "@mui/lab/DatePicker";
+import TextField from "@mui/material/TextField";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
 
 export interface PickerProps {
   onDateChanged: (date: Date) => void;
   initial?: Date;
+  label?: string;
 }
 export function DatePicker(props: PickerProps) {
   const [startDate, setStartDate] = useState(props.initial || new Date());
   return (
-    <Picker
-      selected={startDate}
-      onChange={(date: Date) => {
-        setStartDate(date);
-        props.onDateChanged(date);
-      }}
-    />
+    <LocalizationProvider dateAdapter={DateAdapter}>
+      <Picker
+        label={props.label || "Date"}
+        value={startDate}
+        inputFormat="MM/dd/yyyy"
+        onChange={(d) => {
+          if (!d) {
+            return;
+          }
+          setStartDate(d);
+          props.onDateChanged(d);
+        }}
+        renderInput={(params: any) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
   );
 }
