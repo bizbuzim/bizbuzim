f228700f9c2d3758dd853b8659ffe40227918682
Oleg Sucharevich <oleg2807@gmail.com>
2022-04-13 08:40:16 +0300

    chore(web): update linter

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
-    "react-app"
+    "react-app",
   ],
   plugins: ["@typescript-eslint"],
   env: { browser: true },
@@ -17,9 +17,7 @@ module.exports = {
     "import/no-named-as-default": "off",
     "import/no-named-as-default-member": "off",
     "import/order": ["error", { "newlines-between": "always" }],
-    "no-console": ["error", { allow: ["warn"] }],
-    "react/react-in-jsx-scope": "off"
+    "react/react-in-jsx-scope": "off",
+    "no-console": "warn",
   },
 };
-
-
