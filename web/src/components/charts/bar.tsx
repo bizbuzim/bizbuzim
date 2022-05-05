3e375618ba16067e5607398dab140dd9c8069ee3
Oleg Sucharevich <oleg2807@gmail.com>
2022-05-05 21:50:43 +0300

    chore(web): lint

--- web/src/components/charts/bar.tsx
+++ web/src/components/charts/bar.tsx
@@ -1,9 +1,8 @@
 import _ from "lodash";
-import React, { PureComponent, useContext, useMemo } from "react";
+import { useContext, useMemo } from "react";
 import {
   BarChart,
   Bar,
-  Cell,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -15,50 +14,6 @@ import {
 import { ExpensesContext } from "../../context/expenses";
 import { Expense } from "../../views/expenses/types";
 
-const d = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 export function ExpensesBarChart() {
   const { expenses, isLoading } = useContext(ExpensesContext);
   const data = useMemo(() => {
