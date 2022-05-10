6dbde962ceb8e3c8f450a9f2ad86a1b4113e17c7
Oleg Sucharevich <oleg2807@gmail.com>
2022-05-10 18:47:11 +0300

    refactor(web): use bzselect

--- web/src/views/filters.tsx
+++ web/src/views/filters.tsx
@@ -1,14 +1,10 @@
-import { useContext, useMemo, useState } from "react";
+import { useContext } from "react";
 import _ from "lodash";
 import styled from "styled-components";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import Chip from "@mui/material/Chip";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import Box from "@mui/material/Box";
-import MenuItem from "@mui/material/MenuItem";
 import { BsFilter } from "react-icons/bs";
 import TextField from "@mui/material/TextField";
 
+import BZSelect from "../components/filters/select";
 import { ExpensesContext } from "../context/expenses";
 
 import { DatePicker } from "./../components/date-picker";
@@ -27,17 +23,6 @@ const FilterContainer = styled.div`
   max-height: 5em;
 `;
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
 export interface Props {
   fromDate: Date;
   fromDateChange: (d: Date) => void;
@@ -55,11 +40,7 @@ export function Filters({
   searchChange,
 }: Props) {
   const { isLoading, error, tags } = useContext(ExpensesContext);
-  const [selectedTags, setSelectedTags] = useState<string[]>([]);
-  const uniqueTags = useMemo(
-    () => tags.map((t) => Object.assign({}, { value: t, label: t })),
-    [tags]
-  );
+
   if (isLoading) {
     return <div>loading</div>;
   }
@@ -96,36 +77,7 @@ export function Filters({
           />
         </FilterContainer>
         <FilterContainer>
-          <Select
-            style={{
-              minWidth: "9rem",
-            }}
-            multiple
-            value={selectedTags}
-            onChange={(event: SelectChangeEvent<string[]>) => {
-              const {
-                target: { value },
-              } = event;
-              const r = typeof value === "string" ? value.split(",") : value;
-              tagsSelected(r);
-              setSelectedTags(r);
-            }}
-            input={<OutlinedInput />}
-            renderValue={(selected: string[]) => (
-              <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                {selected.map((value: string) => (
-                  <Chip key={value} label={value} />
-                ))}
-              </Box>
-            )}
-            MenuProps={MenuProps}
-          >
-            {uniqueTags.map((tag, i) => (
-              <MenuItem key={tag.label} value={tag.value}>
-                {tag.label}
-              </MenuItem>
-            ))}
-          </Select>
+          <BZSelect items={tags} onItemSelected={tagsSelected} />
         </FilterContainer>
       </FiltersContainer>
     </>
