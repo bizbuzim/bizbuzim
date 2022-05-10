13093376ccf4329f434eaa33fb757388e42a4593
Oleg Sucharevich <oleg2807@gmail.com>
2022-05-10 21:04:55 +0300

    feat(web): config tag color

--- web/src/context/expenses.ts
+++ web/src/context/expenses.ts
@@ -2,11 +2,16 @@ import { createContext } from "react";
 
 import { Expense } from "./../views/expenses/types";
 
+interface tag {
+  name: string;
+  color: string;
+}
+
 export const ExpensesContext = createContext<{
   isLoading: boolean;
   error?: string;
   expenses: Expense[];
-  tags: string[];
+  tags: tag[];
   payments: string[];
 }>({
   isLoading: true,
