5f9a662c21b0f9ff47e40e749a476a719841c65c
Oleg Sucharevich <oleg2807@gmail.com>
2022-05-19 16:07:37 +0300

    feat(web): use ssl protocol

--- web/src/config.ts
+++ web/src/config.ts
@@ -6,6 +6,6 @@ const {
 } = process.env;
 
 export const BaseURL =
-  "http://" + REACT_APP_HASURA_HOST + ":" + REACT_APP_HASURA_PORT;
+  "https://" + REACT_APP_HASURA_HOST + ":" + REACT_APP_HASURA_PORT;
 export const Auth0Domain = REACT_APP_AUTH0_DOMAIN;
 export const Auth0ClientId = REACT_APP_AUTH0_CLIENT_ID;
