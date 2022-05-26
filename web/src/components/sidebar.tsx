dfa233d78a08ab5633b05332f26352c6ad01bdab
Oleg Sucharevich <oleg2807@gmail.com>
2022-05-26 15:19:27 +0300

    feat(web): collapse and expend sidebar

--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -1,44 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ProSidebar,
   Menu,
   MenuItem,
   SidebarFooter,
   SidebarContent,
+  SidebarHeader,
 } from "react-pro-sidebar";
 import { Link } from "react-router-dom";
 import "react-pro-sidebar/dist/css/styles.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "@mui/material/Button";
 import LogoutIcon from "@mui/icons-material/Logout";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import HomeIcon from "@mui/icons-material/Home";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import SourceIcon from "@mui/icons-material/Source";
 
 export const Sidebar: React.FC = () => {
   const { logout } = useAuth0();
+  const [collapsed, setCollapsed] = useState(false);
+  const collapsedExpendIcon = collapsed ? (
+    <ArrowRightIcon fontSize="large" onClick={() => setCollapsed(false)} />
+  ) : (
+    <ArrowLeftIcon fontSize="large" onClick={() => setCollapsed(true)} />
+  );
   return (
-    <ProSidebar>
+    <ProSidebar collapsed={collapsed}>
+      <SidebarHeader style={{ display: "flex", justifyContent: "flex-end" }}>
+        {collapsedExpendIcon}
+      </SidebarHeader>
       <SidebarContent>
         <Menu iconShape="square">
-          <MenuItem>
+          <MenuItem icon={<HomeIcon />}>
             <Link to={"/"}>Home</Link>
           </MenuItem>
-          <MenuItem>
+          <MenuItem icon={<AccountBalanceIcon />}>
             <Link to={"expenses"}>Expenses</Link>
           </MenuItem>
-          <MenuItem>
+          <MenuItem icon={<SourceIcon />}>
             <Link to={"sources"}>Source</Link>
           </MenuItem>
         </Menu>
       </SidebarContent>
-      <SidebarFooter style={{ textAlign: "center" }}>
-        <Button
-          onClick={() => logout()}
-          variant="contained"
-          endIcon={<LogoutIcon />}
-          style={{ marginTop: "10px", marginBottom: "10px" }}
-        >
-          Log Out
-        </Button>
-      </SidebarFooter>
+      <Logout collapsed={collapsed} onClick={logout} />
+      <SidebarFooter style={{ textAlign: "center" }}></SidebarFooter>
     </ProSidebar>
+  );
+};
+
+const Logout: React.FC<{ collapsed: boolean; onClick(): void }> = ({
+  collapsed,
+  onClick,
+}) => {
+  if (collapsed) {
+    return <LogoutIcon onClick={() => onClick()} />;
+  }
+  return (
+    <Button
+      onClick={() => onClick()}
+      variant="contained"
+      endIcon={<LogoutIcon />}
+      style={{ marginTop: "10px", marginBottom: "10px" }}
+    >
+      Log Out
+    </Button>
   );
 };
