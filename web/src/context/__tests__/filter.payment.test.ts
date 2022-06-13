47178f6845dc46a8287785f7f23e2ceefa583f58
Ortal Sucharevich <ortalsucharevich@Ortals-MacBook-Air.local>
2022-06-13 14:25:02 +0300

    fix pr notes

--- web/src/context/__tests__/filter.payment.test.ts
+++ web/src/context/__tests__/filter.payment.test.ts
@@ -3,20 +3,8 @@ import { Chance } from "chance";
 import { applyFilterPayments } from "../filters";
 import { Expense } from "../../views/expenses/types";
 
+import { generate } from "./filter.tag.test";
 const chance = new Chance();
-const tags = ["loan", "house", "pets", "kids", "personal", "car"];
-const payments = ["cash", "oleg-credit", "ortal-credit", "sibus"];
-
-function generate(): Expense {
-  return {
-    id: chance.guid({ version: 4 }),
-    name: chance.name(),
-    created_at: chance.timestamp().toLocaleString(),
-    payment: payments[chance.integer({ min: 0, max: 1 })],
-    price: chance.integer({ min: 10, max: 1000 }).toString(),
-    tags: chance.pickset(tags, chance.integer({ min: 0, max: tags.length })),
-  };
-}
 
 describe("filter expenses by payments", () => {
   it("should return expenses related to selected payments", () => {
