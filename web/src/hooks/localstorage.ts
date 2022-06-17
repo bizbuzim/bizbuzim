591d414a38fa0acaf4f468d80e23a74b7583a117
Oleg Sucharevich <oleg2807@gmail.com>
2022-06-17 11:58:03 +0300

    feat: add useLocalStorage

---
+++ web/src/hooks/localstorage.ts
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T) => void] {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    const res = window.localStorage.getItem(key);
+    if (!res) {
+      return initialValue;
+    }
+    return JSON.parse(res) as T;
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value: T) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+  return [storedValue || initialValue, setValue];
+}
