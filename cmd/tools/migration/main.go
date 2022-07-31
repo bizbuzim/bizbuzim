6f093aef0ff6dd956bb614ceae187706ec5c0032
Oleg Sucharevich <oleg2807@gmail.com>
2022-07-31 15:49:46 +0300

    feat(migration): run migrations on newly db and generate code

--- cmd/tools/migration/main.go
+++ cmd/tools/migration/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/olegsu/bizbuzim/pkg/fatal"
@@ -19,9 +21,28 @@ func main() {
 	wd, err := os.Getwd()
 	dieOnError(err, "")
 
+	postgresURL := os.Getenv("POSTGRESQL_URL")
+	postgresHost := os.Getenv("POSTGRES_HOST")
+	if postgresURL == "" {
+		lgr.Info("POSTGRESQL_URL is not set, starting postgresql in docker")
+		pg := []string{
+			"run", "-it",
+			"-d",
+			"-p", "5432:5432",
+			"-e", "POSTGRES_PASSWORD=postgres",
+			"postgres:14",
+		}
+		_, err := run(context.Background(), lgr, time.Second*10, "docker", pg...)
+		dieOnError(err, "failed to start postgres")
+		lgr.Info("postgresql started, waiting 10 seconds")
+		time.Sleep(time.Second * 30)
+		postgresURL = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
+		postgresHost = "localhost"
+	}
+
 	p := os.Getenv("MIGRATION_TABLES")
 	if p == "" {
-		p = path.Join(wd, "cmd/tools/migrations/tables.yaml")
+		p = path.Join(wd, "cmd/tools/migration/tables.yaml")
 	}
 	b, err := ioutil.ReadFile(p)
 	dieOnError(err, "failed to read tables.yaml file")
@@ -36,12 +57,14 @@ func main() {
 	}
 	migration := []string{
 		"-path", migrationsPath,
-		"-database", os.Getenv("POSTGRESQL_URL"),
+		"-database", postgresURL,
 	}
 
 	version, err := run(context.Background(), lgr, time.Second*30, "migrate", append(migration, "version")...)
-	dieOnError(err, "failed to get current migration version")
-	lgr.Info("migrating to latest version", "current-version", string(version))
+	if err != nil {
+		lgr.Info("failed to get migration version", "error", err.Error(), "output", string(version))
+	}
+	lgr.Info("migrating to latest version")
 
 	_, err = run(context.Background(), lgr, time.Second*30, "migrate", append(migration, "up")...)
 	dieOnError(err, "failed to run migration")
@@ -49,7 +72,7 @@ func main() {
 
 	dump := []string{
 		"-s",
-		"--host", os.Getenv("POSTGRES_HOST"),
+		"--host", postgresHost,
 		"-U", "postgres",
 		"--port", "5432",
 		"-d", "postgres",
@@ -64,16 +87,33 @@ func main() {
 		}
 	}
 	schema := os.Getenv("SCHEMA_SQL_PATH")
-	if schema == "" {
-		lgr.Info("SCHEMA_SQL_PATH is not set, skipping pg_dump")
-		return
+	if schema != "" {
+		d, err := run(context.Background(), lgr, time.Second*30, "pg_dump", dump...)
+		lgr.Info("pg_dump completed", "distination", schema)
+		dieOnError(err, "failed to run pg_dump")
+		err = os.WriteFile(schema, d, os.ModePerm)
+		dieOnError(err, schema)
 	}
-	d, err := run(context.Background(), lgr, time.Second*30, "pg_dump", dump...)
-	lgr.Info("pg_dump completed", "distination", schema)
-	dieOnError(err, "failed to run pg_dump")
 
-	err = os.WriteFile(schema, d, os.ModePerm)
-	dieOnError(err, schema)
+	lgr.Info("Starting to generate code")
+	xo := []string{
+		"schema", postgresURL,
+		"-o", "pkg/dal",
+		"--go-pkg", "dal",
+		"-j",
+	}
+	for _, object := range tables {
+		for k, v := range object {
+			if k == "name" {
+				xo = append(xo, []string{
+					"-i", v,
+				}...)
+			}
+		}
+	}
+	out, err := run(context.Background(), lgr, time.Second*30, "xo", xo...)
+	dieOnError(err, "failed to run xo")
+	fmt.Println(string(out))
 }
 
 func dieOnError(err error, msg string) {
@@ -81,7 +121,7 @@ func dieOnError(err error, msg string) {
 }
 
 func run(ctx context.Context, lgr *logger.Logger, timeout time.Duration, name string, args ...string) ([]byte, error) {
-	lgr.Info("running cmd", "cmd", name, "args", args)
+	lgr.Info("running cmd", "cmd", name, "args", strings.Join(args, " "))
 	tctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 	cmd := exec.CommandContext(tctx, name, args...)
